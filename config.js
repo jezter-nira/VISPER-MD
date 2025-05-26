48ed9e90d7d2fa0b40837c09ff7f97aa84f37a6c
jezter-nira <charithniroshana7@gmail.com>
2025-05-26 13:07:53 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=cJRkyKqY#b-Rs3xxVuRf6N-vwR5eWFx0DMQ05Lkg5oIXfUH-fluM' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=tdh1kLgK#QHRgQZnh4_O1-VYREkVu-tvfbbi_oAOHeHgtqKj6s4s' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
